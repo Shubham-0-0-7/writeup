2da4170c58fd0c5847b0ac42f8cd0ea3f2360c17
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2026-04-12 14:37:15 +0530

    added links to bandit

--- src/pages/bandit.js
+++ src/pages/bandit.js
@@ -95,9 +95,35 @@ export default function Bandit() {
             Bandit Level 23 → 24
           </Link>
 
+        <Link to="/writeup/blog/2026/04/12/bandit-24-25" className="writeup-card">
+            Bandit Level 24 → 25
+          </Link>
 
+          <Link to="/writeup/blog/2026/04/12/bandit-25-26" className="writeup-card">
+            Bandit Level 25 → 26
+          </Link>
 
+          <Link to="/writeup/blog/2026/04/12/bandit-26-27" className="writeup-card">
+            Bandit Level 26 → 27
+          </Link>
 
+          <Link to="/writeup/blog/2026/04/12/bandit-27-28" className="writeup-card">
+            Bandit Level 27 → 28
+          </Link>
+
+          <Link to="/writeup/blog/2026/04/12/bandit-28-29" className="writeup-card">
+            Bandit Level 28 → 29
+          </Link>
+
+          <Link to="/writeup/blog/2026/04/12/bandit-29-30" className="writeup-card">
+            Bandit Level 29 → 30
+          </Link>
+
+          <Link to="/writeup/blog/2026/04/12/bandit-30-31" className="writeup-card">
+            Bandit Level 30 → 31
+          </Link>
+
+  
 
           <Link to="/writeup/blog/2025/10/18/bandit-passwords" className="writeup-card">
             Bandit Passwords (Complete Dump)
